7d0df7c57fd2f678b5517a38f5e1e294fc59f4d1
Wesley <wesley.contato@hotmail.com>
2020-11-03 06:51:40 -0300

    Testando serviço de leilão

--- leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuariosWSTest.java
+++ leiloes-rest-assured/src/br/com/caelum/leilao/teste/UsuariosWSTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.path.xml.XmlPath;
 
+import br.com.caelum.leilao.modelo.Leilao;
 import br.com.caelum.leilao.modelo.Usuario;
 
 public class UsuariosWSTest {
@@ -55,22 +56,21 @@ JsonPath path = given()
 		assertEquals(mauricio, usuario);
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@Test
+    public void deveRetornarUmLeilao() {
+        JsonPath path = given()
+                .parameter("leilao.id", 1)
+                .header("Accept", "application/json")
+                .get("/leiloes/show")
+                .andReturn().jsonPath();
+
+        Leilao leilao = path.getObject("leilao", Leilao.class);
+
+        Usuario mauricio = new Usuario(1l, "Mauricio Aniche", "mauricio.aniche@caelum.com.br");
+        Leilao esperado = new Leilao(1l, "Geladeira", 800.0, mauricio, false);
+
+        assertEquals(esperado, leilao);
+    }
 	
 	
 	
